fc06525fafe014b17248af3d7c6dcfff2ce75df4
Mohammad <114297179+M-Tinati@users.noreply.github.com>
2022-12-13 19:40:28 +0330

    part of list item

---
+++ pynative/list/Exercise-1.py
@@ -0,0 +1,3 @@
+list1 = [100, 200, 300, 400, 500]
+list1.sort(reverse=True)
+print(list1)
---
+++ pynative/list/Exercise-2.py
@@ -0,0 +1,8 @@
+list1 = ["M", "na", "i", "Ke"]
+list2 = ["y", "me", "s", "lly"]
+
+for i in zip(list1,list2):
+    for j in i:
+        print(j ,  end="")
+    
+    
